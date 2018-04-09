0df669443ae2b31f08350dc1f4a0c2f98ef893cd
daobee <chjr93820@gmail.com>
2018-04-09 09:04:22 +0800

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-b717e4a7066510b34719.js","a9e9e5b63c58e41e25616cc4649ea72d"],["/app-ba0f507945697c1e0120.js","9f9172043e358c89c6a571cc0bac5d73"],["/commons-87965dbda70c844d4255.js","b0bd2a2a53432f76e73e6068849dc0ee"],["/index.html","8e5ce21b48e2d743ecf6668d14b70bce"],["/offline-plugin-app-shell-fallback/index.html","c4d883885f2233e4645ab637341e5c66"]];
+var precacheConfig = [["/app-b717e4a7066510b34719.js","a9e9e5b63c58e41e25616cc4649ea72d"],["/app-ba0f507945697c1e0120.js","9f9172043e358c89c6a571cc0bac5d73"],["/commons-87965dbda70c844d4255.js","b0bd2a2a53432f76e73e6068849dc0ee"],["/index.html","a9361d48869107303ac1d17ba0b8321b"],["/offline-plugin-app-shell-fallback/index.html","3cd5f1218d0da3e29ecfeb2928bdd701"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
